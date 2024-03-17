6fce3ce325d90d6b378e963126726afd4862d3b9
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-03-17 16:55:35 +0000

    adding next shows route

--- Server/server/app.py
+++ Server/server/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, jsonify, request
 import uuid
 import sqlite3
@@ -25,8 +27,8 @@ def get_db():
     return conn, conn.cursor()
 
 
-# Register and Update User route
-@app.route('/register_user', methods=['PUT'])
+# Register User route
+@app.route('/register_user', methods=['POST'])
 def register_user():
     # Get data from request
     data = request.json
@@ -35,6 +37,10 @@ def register_user():
     password = data.get('password')
     tax_number = data.get('tax_number')
     public_key = data.get('public_key')
+    credit_card_number = data.get('credit_card_number')
+    credit_card_validity = data.get('credit_card_validity')
+    credit_card_type = data.get('credit_card_type')
+
 
     # Check if all required fields are present
     if not name or not username or not password or not tax_number or not public_key:
@@ -45,20 +51,24 @@ def register_user():
     cursor.execute('SELECT * FROM customer WHERE username = ?', (username,))
     customer = cursor.fetchone()
     if customer:
-        # Update customer info
-        cursor.execute('UPDATE customer SET name = ?, password = ?, tax_number = ?, public_key = ? WHERE username = ?',
-                       (name, password, tax_number, public_key, username))
-        conn.commit()
-        conn.close()
-        return jsonify({'message': 'Customer updated successfully'}), 200
+        # Return customer exists message
+        return jsonify({'message': 'Customer already exists'}), 409
 
+    customer_id = str(uuid.uuid4())
     # Insert new customer
-    cursor.execute('INSERT INTO customer (name, username, password, tax_number, public_key) VALUES (?, ?, ?, ?, ?)',
-                   (name, username, password, tax_number, public_key))
+    cursor.execute('INSERT INTO customer (CUSTOMER_ID, USERNAME, PASSWORD, TAX_NUMBER, PUBLIC_KEY, NAME) VALUES (?, ?, ?, ?, ?, ?)',
+                   (customer_id, username, password, tax_number, public_key, name))
+
+    # Insert new credit card
+    cursor.execute('INSERT INTO credit_card (CUSTOMER_ID, TYPE, NUMBER, VALIDITY) VALUES (?, ?, ?, ?)',
+                   (customer_id, credit_card_type, credit_card_number, credit_card_validity))
     conn.commit()
     conn.close()
 
-    return jsonify({'message': 'Customer registered successfully'}), 201
+    return jsonify({
+                        'message': 'Customer registered successfully',
+                        'customer_id': customer_id
+                    }), 201
 
 # Login Route
 @app.route('/login', methods=['POST'])
@@ -82,6 +92,26 @@ def login():
         return jsonify({'error': 'Invalid username or password'}), 401
 
     return jsonify({'message': 'Login successful'}), 200
+
+# Next Shows Route
+@app.route('/next_shows', methods=['GET'])
+def next_shows():
+    args = request.args
+
+    nr_of_shows = args['shows']
+
+    # Query next nr_of_shows
+    conn, cursor = get_db()
+    cursor.execute('SELECT * FROM EVENT as e where e.DATE >= date() limit ?', nr_of_shows)
+    shows = cursor.fetchall()
+    conn.close()
+
+    # If no shows are found
+    if not shows:
+        return jsonify({'message': 'No shows found'}), 404
+
+    return jsonify([dict(show) for show in shows]), 200
+
 
 
 # Initialize database
