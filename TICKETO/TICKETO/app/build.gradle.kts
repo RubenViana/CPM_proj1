221f1289b0b322ff7dc6763fa7495d38b3241af4
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-04-07 22:25:08 +0100

    adding some utils and dependencies

--- TICKETO/TICKETO/app/build.gradle.kts
+++ TICKETO/TICKETO/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+//    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 android {
@@ -61,11 +63,12 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material-icons-extended")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.zxing:core:3.5.3")
+
 }
---
+++ TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/utils/Serialization.kt
@@ -0,0 +1,34 @@
+package org.feup.ticketo.utils
+
+import com.android.volley.VolleyError
+import com.google.gson.Gson
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.json.JSONObject
+
+fun getServerResponseErrorMessage (error: VolleyError): String? {
+    return Json.decodeFromString<HashMap<String, String>>(error.networkResponse.data.decodeToString())["error"]
+}
+inline fun <reified T> byteArrayToObject(byteArray: ByteArray): T {
+    return Json.decodeFromString(byteArrayToString(byteArray))
+}
+
+fun byteArrayToString(byteArray: ByteArray): String {
+    return byteArray.toString(Charsets.UTF_8)
+}
+
+inline fun <reified T : Any> objectToByteArray(data: T) : ByteArray {
+    return stringToByteArray(objectToJsonString(data))
+}
+
+inline fun <reified T : Any> objectToJsonString(data: T): String {
+    return Json.encodeToString(data)
+}
+
+fun stringToByteArray(input: String): ByteArray {
+    return input.toByteArray(Charsets.UTF_8)
+}
+
+inline fun <reified T : Any>  objectToJson (data: T): JSONObject {
+    return JSONObject(Gson().toJson(data))
+}
