41fbe01d98bfc18ff83ccb25d1e8f0c703e9e2af
GuerrieraDellaLuna <chaimaamounir3@gmail.com>
2024-04-10 03:52:08 +0100

    started working on the orders screen

--- TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/ui/screens/orders/OrdersScreen.kt
+++ TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/ui/screens/orders/OrdersScreen.kt
@@ -1,16 +1,67 @@
 package org.feup.ticketo.ui.screens.orders
 
+import android.view.MenuItem
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun OrdersScreen(navController: NavHostController, viewModel: OrdersViewModel) {
     Column(
-        Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
     ) {
+        Text(text = "Cafeteria Menu", modifier = Modifier.padding(16.dp))
 
+        // Display menu items
+        LazyColumn {
+            items(menuItems) { menuItem ->
+                MenuItemCard(menuItem)
+            }
+        }
+
+        // Add Voucher Button
+       /* if (vouchers.isNotEmpty()) {
+            Button(
+                onClick = { /* Add voucher to the order */ },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Add Voucher")
+            }
+        }*/
+
+        // Confirm Order Button
+        Button(
+            onClick = { /* Confirm the order */ },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Confirm Order")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuItemCard(menuItem: MenuItem) {
+    OutlinedCard(
+        modifier = Modifier.padding(16.dp),
+        colors = CardDefaults.outlinedCardColors(),
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+        ) {
+            Text(text = menuItem.name)
+            Text(text = "Price: ${menuItem.price}")
+            Text(text = menuItem.description)
+        }
     }
 }
