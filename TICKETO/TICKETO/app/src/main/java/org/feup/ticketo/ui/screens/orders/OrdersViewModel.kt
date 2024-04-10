968cae60f45c6f02bd7e1fcc6c186b84511627a2
GuerrieraDellaLuna <chaimaamounir3@gmail.com>
2024-04-10 03:57:10 +0100

    adding orders view model need some patching

--- TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/ui/screens/orders/OrdersViewModel.kt
+++ TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/ui/screens/orders/OrdersViewModel.kt
@@ -1,8 +1,23 @@
 package org.feup.ticketo.ui.screens.orders
 
+import android.content.Context
+import android.view.MenuItem
 import androidx.lifecycle.ViewModel
+import org.feup.ticketo.data.storage.TicketoDatabase
 
 class OrdersViewModel(
-
+    private val context: Context
 ) : ViewModel() {
+    private val db = TicketoDatabase.getDatabase(context = context)
+
+    fun getMenuItems(): List<MenuItem> {
+        return db.ticketoDao().getAllProducts().map {
+            MenuItem(
+                productId = it.product_id ?: 0L,
+                name = it.name ?: "",
+                description = it.description ?: "",
+                price = it.price ?: 0.0f
+            )
+        }
+    }
 }
