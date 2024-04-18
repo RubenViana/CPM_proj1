e2fcb27623335e8c6646a115121c1c39dddd2d1c
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-04-18 18:51:36 +0100

    adding states to EventTicketsViewModel

--- TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/ui/components/errorToasts.kt
+++ TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/ui/components/errorToasts.kt
@@ -7,12 +7,12 @@ import com.android.volley.VolleyError
 import org.feup.ticketo.utils.getServerResponseErrorMessage
 
 @Composable
-fun serverErrorToast(start: String, error: VolleyError) {
+fun serverErrorToast(start: String, error: VolleyError?) {
     val errorMessage = getServerResponseErrorMessage(error) ?: "Unknown Error"
     Toast.makeText(LocalContext.current, "$start: $errorMessage", Toast.LENGTH_LONG).show()
 }
 
 @Composable
-fun errorToast(start:String, error: String) {
+fun errorToast(start: String, error: String) {
     Toast.makeText(LocalContext.current, "$start: $error", Toast.LENGTH_LONG).show()
 }
