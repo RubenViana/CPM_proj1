28f1a8495206f24b87df68baec21a74a0e36f08d
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-04-22 22:27:06 +0100

    removing some gradle dependencies

--- TICKETO/TICKETO/app/build.gradle.kts
+++ TICKETO/TICKETO/app/build.gradle.kts
@@ -93,16 +93,16 @@ dependencies {
     implementation("androidx.room:room-rxjava2:$room_version")
 
     // optional - RxJava3 support for Room
-    implementation("androidx.room:room-rxjava3:$room_version")
+//    implementation("androidx.room:room-rxjava3:$room_version")
 
     // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation("androidx.room:room-guava:$room_version")
+//    implementation("androidx.room:room-guava:$room_version")
 
     // optional - Test helpers
-    testImplementation("androidx.room:room-testing:$room_version")
+//    testImplementation("androidx.room:room-testing:$room_version")
 
     // optional - Paging 3 Integration
-    implementation("androidx.room:room-paging:$room_version")
+//    implementation("androidx.room:room-paging:$room_version")
 
 
 
