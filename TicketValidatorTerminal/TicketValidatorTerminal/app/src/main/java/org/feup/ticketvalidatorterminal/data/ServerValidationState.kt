b8832931fa4f1e495e61984d0ada678b20daab41
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-04-23 20:44:55 +0100

    Delete TicketValidatorTerminal/TicketValidatorTerminal directory

--- TicketValidatorTerminal/TicketValidatorTerminal/app/src/main/java/org/feup/ticketvalidatorterminal/data/ServerValidationState.kt
+++
@@ -1,8 +0,0 @@
-package org.feup.ticketvalidatorterminal.data
-
-import com.android.volley.VolleyError
-
-open class ServerValidationState {
-    data object Success : ServerValidationState()
-    data class Failure(val error: VolleyError) : ServerValidationState()
-}
