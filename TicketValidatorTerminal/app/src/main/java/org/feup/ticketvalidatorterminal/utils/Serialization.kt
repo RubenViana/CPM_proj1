021cb6b7b81c86a413d3c489516f676bf815ad71
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-04-23 23:12:34 +0100

    Update Serialization.kt

--- TicketValidatorTerminal/app/src/main/java/org/feup/ticketvalidatorterminal/utils/Serialization.kt
+++ TicketValidatorTerminal/app/src/main/java/org/feup/ticketvalidatorterminal/utils/Serialization.kt
@@ -1,4 +1,4 @@
-package org.feup.ticketo.utils
+package org.feup.ticketvalidatorterminal.utils
 
 import com.android.volley.VolleyError
 import com.google.gson.Gson
@@ -6,19 +6,9 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.json.JSONObject
 
-<<<<<<<< HEAD:TICKETO/TICKETO/app/src/main/java/org/feup/ticketo/utils/Serialization.kt
-fun getServerResponseErrorMessage(error: VolleyError?): String? {
-    try {
-        return Json.decodeFromString<HashMap<String, String>>(error!!.networkResponse.data.decodeToString())["message"]
-    } catch (e: Exception) {
-        return "Server is offline."
-    }
-========
 fun getServerResponseErrorMessage (error: VolleyError): String? {
     return Json.decodeFromString<HashMap<String, String>>(error.networkResponse.data.decodeToString())["message"]
->>>>>>>> b8832931fa4f1e495e61984d0ada678b20daab41:TicketValidatorTerminal/app/src/main/java/org/feup/ticketvalidatorterminal/utils/Serialization.kt
 }
-
 inline fun <reified T> byteArrayToObject(byteArray: ByteArray): T {
     return Json.decodeFromString(byteArrayToString(byteArray))
 }
@@ -27,7 +17,7 @@ fun byteArrayToString(byteArray: ByteArray): String {
     return byteArray.toString(Charsets.UTF_8)
 }
 
-inline fun <reified T : Any> objectToByteArray(data: T): ByteArray {
+inline fun <reified T : Any> objectToByteArray(data: T) : ByteArray {
     return stringToByteArray(objectToJsonString(data))
 }
 
@@ -39,6 +29,6 @@ fun stringToByteArray(input: String): ByteArray {
     return input.toByteArray(Charsets.UTF_8)
 }
 
-inline fun <reified T : Any> objectToJson(data: T): JSONObject {
+inline fun <reified T : Any>  objectToJson (data: T): JSONObject {
     return JSONObject(Gson().toJson(data))
 }
