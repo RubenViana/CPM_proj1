f6ba9117777966120e2638794e281c002ec4b9e7
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-04-23 23:14:31 +0100

    Delete TicketValidatorTerminal directory

--- TicketValidatorTerminal/app/src/main/java/org/feup/ticketvalidatorterminal/data/ServerValidationState.kt
+++
@@ -1,9 +0,0 @@
-package org.feup.ticketvalidatorterminal.data
-
-import com.android.volley.VolleyError
-import org.json.JSONObject
-
-open class ServerValidationState {
-    data class Success(val response: JSONObject) : ServerValidationState()
-    data class Failure(val error: VolleyError) : ServerValidationState()
-}
--- TicketValidatorTerminal/app/src/main/java/org/feup/ticketvalidatorterminal/utils/ServerCommunication.kt
+++
@@ -1,3 +0,0 @@
-package org.feup.ticketvalidatorterminal.utils
-
-const val serverUrl = "http://10.0.2.2:5000/"
